7d73359331c4a6287157773c3253ad06ca11aa85
Axzurey <66486198+Axzurey@users.noreply.github.com>
2022-09-22 10:18:13 -0400

    moved nueplay

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { authModule } from './routes/auth/auth.module';
-import { NueplayModule } from './nueplay/nueplay.module';
+import { NueplayModule } from './routes/nueplay/nueplay.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/phxtest'), authModule, NueplayModule],
